40b15c011196ea5fac86a4f07dd8cedb45cca4c7
Daniel Sperka <djsperka@ucdavis.edu>
2025-05-20 17:05:04 -0700

    cleanup

--- octv.py
+++ octv.py
@@ -1,28 +1,38 @@
 import sys
-from myengine import setup_logging
-from myengine import setup_logging, StandardEngineParams, BaseEngine, DEFAULT_ENGINE_PARAMS
+from myengine import setup_logging, DEFAULT_ENGINE_PARAMS
 from OCTUi import OCTUi
+from StandardEngineParamsDialog import StandardEngineParamsDialog
 
 #from PyQt5.QtCore import oop, QTimer, QState, QStateMachine
 from PyQt5.QtWidgets import QApplication
 #from PyQt5 import uic
 
+cfg = DEFAULT_ENGINE_PARAMS
+
+setup_logging()
+
+# gui and exception handler
+app = QApplication(sys.argv)
+
+import traceback
+def handler(cls, ex, trace):
+    traceback.print_exception(cls, ex, trace)
+    app.closeAllWindows()
+sys.excepthook = handler
+
+cfgDialog = StandardEngineParamsDialog(cfg)
+
+cfgDialog.accepted.connect(cfgAccepted)
+cfgDialog.show()
 
 
-if __name__ == '__main__':
-    setup_logging()
+    # octui = OCTUi()
+    # octui.show()
 
-    # gui and exception handler
-    app = QApplication(sys.argv)
 
-    import traceback
-    def handler(cls, ex, trace):
-        traceback.print_exception(cls, ex, trace)
-        app.closeAllWindows()
-    sys.excepthook = handler
+sys.exit(app.exec_())
 
-    octui = OCTUi()
 
-    octui.show()
-    #timer.start(1000)
-    sys.exit(app.exec_())
+def cfgAccepted():
+    cfg =  cfgDialog.getEngineParameters()
+    
