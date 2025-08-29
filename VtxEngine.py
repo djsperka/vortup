60f15b2b72e96bf2c25f1ed461f120eafade3eb2
Daniel Sperka <djsperka@ucdavis.edu>
2025-08-29 16:38:03 -0700

    Remove the filesave objects in lieu of the newer SaveVolumeGroupBox.

--- VtxEngine.py
+++ VtxEngine.py
@@ -1,19 +1,14 @@
 from VtxBaseEngine import VtxBaseEngine
-from vortex import Range, get_console_logger as get_logger
-from vortex.marker import Flags
-from vortex.engine import Engine, EngineConfig, Block, dispersion_phasor, StackDeviceTensorEndpointInt8, SpectraStackHostTensorEndpointUInt16, AscanStackEndpoint, SpectraStackEndpoint, VolumeStrobe, SegmentStrobe
-from VtxEngineParams import VtxEngineParams, FileSaveConfig
-from vortex.scan import RasterScanConfig
+from vortex import get_console_logger as get_logger
+from vortex.engine import Engine, EngineConfig, StackDeviceTensorEndpointInt8, SpectraStackHostTensorEndpointUInt16, SpectraStackEndpoint
 from vortex.format import FormatPlanner, FormatPlannerConfig, StackFormatExecutorConfig, StackFormatExecutor, SimpleSlice
-from vortex.storage import HDF5StackUInt16, HDF5StackInt8, HDF5StackConfig, HDF5StackHeader, SimpleStackUInt16, SimpleStackInt8, SimpleStackConfig, SimpleStackHeader
-import numpy as np
+from vortex.storage import SimpleStackUInt16
 import logging
-from typing import Tuple, Union, Any
-#from AcqParams import AcqParams, DEFAULT_ACQ_PARAMS
+from typing import Tuple, Any
 from OCTUiParams import OCTUiParams
 
 class VtxEngine(VtxBaseEngine):
-    def __init__(self, params: OCTUiParams, fcfg_ascans: FileSaveConfig=FileSaveConfig('ascans'), fcfg_spectra: FileSaveConfig=FileSaveConfig('spectra')):
+    def __init__(self, params: OCTUiParams):
 
         cfg = params.vtx
         acq = params.acq 
@@ -22,8 +17,6 @@ class VtxEngine(VtxBaseEngine):
         # base class 
         super().__init__(params.vtx, params.acq)
         self._cfg = params.vtx
-        self._fcfg_ascans = fcfg_ascans
-        self._fcfg_spectra = fcfg_spectra
         self._logger = logging.getLogger(__name__)
         # Base class has stuff made, but no engine constructed:
         # self._acquire
@@ -88,21 +81,11 @@ class VtxEngine(VtxBaseEngine):
         sfe = StackFormatExecutor()
         sfe.initialize(sfec)
 
-        # endpoint for display
+        # endpoint for display of ascans
         vshape = (scfg.bscans_per_volume, scfg.ascans_per_bscan, samples_to_save)
         self._logger.info('Create StackDeviceTensorEndpointInt8 with shape {0:s}'.format(str(vshape)))
         self._endpoint_ascan_display = StackDeviceTensorEndpointInt8(sfe, vshape, get_logger('stack', cfg.log_level))
         endpoints.append(self._endpoint_ascan_display)
-
-        if fcfg_ascans.save:
-            # endpoint for saving ascan data
-            # Will save ascans - a full volume at a time. Save all data acquired!
-            # Shape of data SAVED will be different than the displayed data. Here we save all samples, not half of them.
-            # The storage object 'SimpleStackInt8' doesn't save data until you call open().
-
-            shape = (scfg.bscans_per_volume, scfg.ascans_per_bscan, samples_to_save, 1)
-            self._endpoint_ascan_storage, self._ascan_storage = self.getStorageEndpoint(fcfg_ascans, shape)
-            endpoints.append(self._endpoint_ascan_storage)
 
 
         sfec_spectra = StackFormatExecutorConfig()
@@ -112,12 +95,11 @@ class VtxEngine(VtxBaseEngine):
         self._logger.info('Create SpectraStackHostTensorEndpointUInt16 with shape {0:s}'.format(str(shape_spectra)))
         self._endpoint_spectra_display = SpectraStackHostTensorEndpointUInt16(sfe_spectra, shape_spectra, get_logger('stack', cfg.log_level))
         endpoints.append(self._endpoint_spectra_display)
-        if fcfg_spectra.save:
 
-            # make an endpoint for saving spectra data
-            shape = (scfg.bscans_per_volume, scfg.ascans_per_bscan, acq.samples_per_ascan, 1)
-            self._endpoint_spectra_storage, self._spectra_storage = self.getStorageEndpoint(fcfg_spectra, shape)
-            endpoints.append(self._endpoint_spectra_storage)
+        # make an endpoint for saving spectra data
+        shape = (scfg.bscans_per_volume, scfg.ascans_per_bscan, acq.samples_per_ascan, 1)
+        self._endpoint_spectra_storage, self._spectra_storage = self.getSpectraStorageEndpoint(shape)
+        endpoints.append(self._endpoint_spectra_storage)
 
         #
         # engine setup
@@ -154,71 +136,27 @@ class VtxEngine(VtxBaseEngine):
     def stop(self):
         # only if we are running
         if self._engine and not self._engine.done:
-            if self._fcfg_spectra.save:
-                self._spectra_storage.close()
-            if self._fcfg_ascans.save:
-                self._ascan_storage.close()
             self._engine.stop()
         else:
             self._logger.warning('engine is not running')
             
-    def getStorageEndpoint(self, fcfg: FileSaveConfig, shape) -> Tuple[Any, Any]:
+    def getSpectraStorageEndpoint(self, shape) -> Tuple[Any, Any]:
 
-        # The 'type' of the fcfg should be either 'spectra' or 'ascans'
-        if fcfg.type=='spectra':
+            storage = SimpleStackUInt16(get_logger('npy-spectra', self._cfg.log_level))
 
-            if fcfg.extension == "npy":
-                npsc = SimpleStackConfig()
-                npsc.shape = shape
-                npsc.header = SimpleStackHeader.NumPy
-                npsc.path = fcfg.filename
-                storage = SimpleStackUInt16(get_logger('npy-spectra', self._cfg.log_level))
-                storage.open(npsc)
+            # Executor config has only three properties:
+            #
+            # property erase_after_volume - not sure
+            # property sample_slice  - Collect only a slice of samples from each ascan
+            # property sample_transform - not sure
+            # 
+            # So basically, I'm not sure what the executor's role is here. 
+            # But - when the Endpoint class is created, the executor is the first
+            # arg to the constructor. 
 
-                # Executor config has only three properties:
-                #
-                # property erase_after_volume - not sure
-                # property sample_slice  - Collect only a slice of samples from each ascan
-                # property sample_transform - not sure
-                # 
-                # So basically, I'm not sure what the executor's role is here. 
-                # But - when the Endpoint class is created, the executor is the first
-                # arg to the constructor. 
+            sfec = StackFormatExecutorConfig()
+            sfe = StackFormatExecutor()
+            sfe.initialize(sfec)
+            endpoint_storage = SpectraStackEndpoint(sfe, storage, log=get_logger('npy-spectra', self._cfg.log_level))
 
-                sfec = StackFormatExecutorConfig()
-                sfe = StackFormatExecutor()
-                sfe.initialize(sfec)
-                endpoint_storage = SpectraStackEndpoint(sfe, storage, log=get_logger('npy-spectra', self._cfg.log_level))
-
-                def local_cb(sample_idx, scan_idx, volume_idx):
-                    print("volume_callback ", volume_idx)
-                    print("storage: ", endpoint_storage)
-                    storage.close()
-                endpoint_storage.volume_callback = local_cb
-            else:
-                self._logger.warning('extension {0:s} not supported yet sorry, use npy'.format(fcfg.extension))
-                raise NotImplementedError('extension {0:s} not supported yet sorry, use npy'.format(fcfg.extension))
-
-        elif fcfg.type=='ascans':
-            if fcfg.extension == "npy":
-                npsc = SimpleStackConfig()
-                npsc.shape = shape
-                npsc.header = SimpleStackHeader.NumPy
-                npsc.path = fcfg.filename
-
-                sfec = StackFormatExecutorConfig()
-                sfe = StackFormatExecutor()
-                sfe.initialize(sfec)
-                storage = SimpleStackInt8(get_logger('npy-ascan', self._cfg.log_level))
-                storage.open(npsc)
-                endpoint_storage = AscanStackEndpoint(sfe, storage, log=get_logger('npy-ascan', self._cfg.log_level))
-
-            else:
-                self._logger.warning('extension {0:s} not supported yet sorry, use npy'.format(fcfg.extension))
-                raise NotImplementedError('extension {0:s} not supported yet sorry, use npy'.format(fcfg.extension))
-
-        else:
-            raise ValueError('FileSaveConfig.type must be either ascans or spectra. ''{0:s}'' not supported.'.format(fcfg.type))
-
-
-        return endpoint_storage, storage
+            return endpoint_storage, storage
