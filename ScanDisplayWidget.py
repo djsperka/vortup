0e1fc0d6e70c5cd26083b15dc104b818d0e7f572
Daniel J Sperka <djsperka@ucdavis.edu>
2025-12-01 17:06:02 -0800

    Minor tweak

--- ScanDisplayWidget.py
+++ ScanDisplayWidget.py
@@ -3,6 +3,7 @@ from VtxEngine import VtxEngine
 from PyQt5.QtWidgets import QWidget
 
 class ScanDisplayWidget(QWidget, ABC): 
+
     def __init__(self, parent: QWidget=None):
         super(QWidget).__init__(parent)
 
