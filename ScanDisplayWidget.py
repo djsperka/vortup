ec3c39efbcb4e9b27a995dad899d6b56468722b9
Daniel J Sperka <djsperka@ucdavis.edu>
2025-12-01 17:03:13 -0800

    Initial add.

---
+++ ScanDisplayWidget.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+from VtxEngine import VtxEngine
+from PyQt5.QtWidgets import QWidget
+
+class ScanDisplayWidget(QWidget, ABC): 
+    def __init__(self, parent: QWidget=None):
+        super(QWidget).__init__(parent)
+
+    def 
