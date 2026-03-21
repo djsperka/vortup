c7abf482bfa79429ce90b881eb2bd7e811f202b4
Daniel Sperka <djsperka@ucdavis.edu>
2026-03-20 17:02:24 -0700

    STILL  wip.

--- MultiPlotSelectWidget.py
+++ MultiPlotSelectWidget.py
@@ -1,11 +1,50 @@
-from qtpy.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout
+from qtpy.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QLabel, QPushButton
+from qtpy.QtCore import QTimer, Signal
+from qtpy.QtGui import QPixmap, QImage
 from image_display import ImageDisplay
+from PIL import Image
 
 import numpy as np
 from dataclasses import dataclass
 
-# Inheriting from FigureCanvasQTAgg. 
-# FigureCanvasQTAgg is a widget. Call update() to invalidate and trigger a paintEvent.
+
+
+
+class MyImageWidget(QLabel):
+
+    __clicked_signal = Signal(int, int, name='clicked')
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setScaledContents(True)
+        self.setMinimumSize(256, 256)
+        self._userdata = None
+
+    @property
+    def userdata(self):
+        return self._userdata
+    
+    @userdata.setter
+    def userdata(self, value):
+        self._userdata = value
+
+    def set_image(self, data: np.ndarray):
+        #        qrgb_dict = {'r': lambda x: QtGui.qRgb(x, 0, 0),
+        #              'g': lambda x: QtGui.qRgb(0, x, 0),
+        #              'b': lambda x: QtGui.qRgb(0, 0, x)}
+        # colortable = [qrgb_dict[color](i) for i in range(256)]
+        s=data.shape
+        img = QImage(data, s[1], s[0], s[1], QImage.Format_Indexed8)
+        qpix = QPixmap(QImage(img))
+        self.setPixmap(qpix)
+
+    def mouseReleaseEvent(self, ev):
+        if self.userdata is None:
+            d=(-1,-1)
+        else:
+            d=self.userdata
+        self.__clicked_signal.emit(*d)
+
 
 @dataclass
 class MPSWData:
@@ -26,22 +65,24 @@ class MPSW(QWidget):
         # right side is a single widget
         layout = QGridLayout()
 
-#         for i in range(self._cols):
-#             for j in range(self._rows):
-#                 self._data[i][j] = None
-#                 self._plots[i][j] = NumpyImageViewer()
-#                 grid_layout.addWidget(self._plots[i][j], i, j)
+
+        # a word of warning: the widgets in QGridLayout are specified (0-based) ROW,COL
 
         self._data = [[None for _ in range(self._columns)] for _ in range(self._rows)]
         self._l = [[0 for _ in range(self._columns)] for _ in range(self._rows)]
         for i in range(self._columns):
             for j in range(self._rows):
-                self._l[j][i] = ImageDisplay()
+                self._l[j][i] = MyImageWidget()
+                self._l[j][i].userdata = (j,i)
+                self._l[j][i].clicked.connect(self.image_clicked)
                 layout.addWidget(self._l[j][i], j, i)
-        self._r = ImageDisplay()
+        self._r = MyImageWidget()
         layout.addWidget(self._r, 0, self._columns, self._columns, self._rows)
 
         self.setLayout(layout)
+
+    def image_clicked(self, c, r):
+        print("Image clicked c={:d} r={:d}".format(c, r))
 
     def add_data(self, ascan_data, spectra_data):
         # find first empty slot, save it there
@@ -49,7 +90,7 @@ class MPSW(QWidget):
             for j in range(self._rows):
                 if self._data[j][i] is None:
                     self._data[j][i] = MPSWData(ascan_data, spectra_data)
-                    print("Saved at ", i, j, np.shape(ascan_data))
+                    self._l[j][i].set_image(ascan_data)
                     break
             else:
                 # The else here is executed ONLY IF THERE WAS NO BREAK!
@@ -115,19 +156,44 @@ class MPSW(QWidget):
 #         super().__init__(fig)
 #         self.setParent(parent)
 
+
+
+
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, list=[]):
         super().__init__()
+        self._image_list = list
+        self._image_index = 0
         self.setWindowTitle("PyQt5 Matplotlib 2D Image Example")
-
-        self.plot = MPSW(self, rows=2, columns=2)
-
+        self.plot = MPSW(self, rows=3, columns=3)
         self.setCentralWidget(self.plot)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timeout)
+        self.timer.start(1000)  # might not actually start until event loop starts
+
+    def timeout(self):
+        if len(self._image_list) > 0:
+            image = Image.open(self._image_list[self._image_index])
+            # convert image to numpy array
+            data = np.asarray(image)
+            self.plot.add_data(data, data.shape)    # the data.shape is dummy - should be spectra_data that might be saved
+            self._image_index+=1
+            if self._image_index >= len(self._image_list):
+                self._image_index = 0
+        else:
+            print('no images')
+
+
 
 import sys
 if __name__ == "__main__":
+
+    # load image filenames
+    with open('images.txt', 'r') as f:
+        image_list = [line.rstrip() for line in f]
+
     app = QApplication(sys.argv)
-    window = MainWindow()
+    window = MainWindow(image_list)
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
 
